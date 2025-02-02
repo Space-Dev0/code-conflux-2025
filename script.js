589d4d831a4a379cbfb0ea9c2dfa7728fe87f3ec
Space-Dev0 <179112172+Space-Dev0@users.noreply.github.com>
2025-02-02 21:59:27 +0530

    feat: deleted fuckery

--- script.js
+++
@@ -1,6 +0,0 @@
-function confirmRefresh() {
-    const isConfirmed = confirm("Are you sure you want to start a new resume? All unsaved changes will be lost.");
-    if (isConfirmed) {
-        window.location.reload();
-    }
-}
